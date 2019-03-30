9e371d6bdc525b9c8af17668851dd75a57132ac5
lz670328 <827528477@qq.com>
2019-03-30 11:08:50 +0800

    第一次提交

---
+++ day05/blackbbb/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+
+}
+</script>
+
+<style>
+
+</style>
